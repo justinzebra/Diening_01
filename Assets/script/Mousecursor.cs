cf1a4cdd262b6da01f2e6a572c79a275a347d1d4
justinzebra <39110422+justinzebra@users.noreply.github.com>
2020-11-08 23:06:26 +0800

    20201108_wei_23.06

---
+++ Assets/script/Mousecursor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mousecursor : MonoBehaviour
+{
+    public Texture2D mouseeye;
+    public Texture2D mousehand;
+
+    void Start()
+    {
+
+          //Cursor.visible = false;
+        Cursor.SetCursor(mousehand, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        // Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // transform.position = cursorPos;
+    }
+
+
+    void OnmouseEnter()
+    {
+        Cursor.SetCursor(mouseeye, Vector2.zero, CursorMode.ForceSoftware);
+
+
+    }
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(mousehand, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+}
+
