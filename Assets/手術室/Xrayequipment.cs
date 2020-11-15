4d4af682fa72a72f8c6946f69ca6fc419144de05
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2020-11-15 15:05:53 +0800

    20201115.1505

---
+++ Assets/手術室/Xrayequipment.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xrayequipment : MonoBehaviour
+{
+   public GameObject monitor;
+    public Player AA;
+    void OnMouseDown()
+    {
+        if(gameObject.name=="X光螢幕")
+        {
+            monitor.SetActive(true);
+            AA.StopMove();
+        }
+    }
+    void CloseMonitor()
+    {
+        if(Input.GetKey(KeyCode.H))
+        {
+            AA.ReStartMove();
+            monitor.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        monitor.SetActive(false);
+        AA = AA.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CloseMonitor();
+    }
+}
---
+++ Assets/手術室/bodyBack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodyBack : MonoBehaviour
+{
+    public GameObject monitor;
+    public Player AA;
+    public void OnMouseDown()
+    {
+        if (gameObject.name == "backbutton")
+        {
+            monitor.SetActive(false);
+            AA.ReStartMove();
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        AA = AA.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
