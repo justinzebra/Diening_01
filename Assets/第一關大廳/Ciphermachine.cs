5c1ac170b5749cc8beeac65e8fee3f1a292f7f3b
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2020-11-22 22:43:46 +0800

    2020112222.43

---
+++ Assets/第一關大廳/Ciphermachine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ciphermachine : MonoBehaviour
+{
+    public Player AA;
+    BoxCollider2D bw;
+    SpriteRenderer w;
+    public GameObject cipher;
+    
+    void Start()
+    {
+        AA = AA.GetComponent<Player>();
+        bw = GetComponent<BoxCollider2D>();
+        w = GetComponent<SpriteRenderer>();
+        cipher.SetActive(false);
+        
+    }
+    public void CloseBoxCollider()
+    {
+        bw.enabled = false;
+        w.enabled = false;
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.name == "cipher")
+        {
+            AA.StopMove();
+            cipher.SetActive(true);
+            
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
