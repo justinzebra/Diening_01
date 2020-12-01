0ec45c98e9c48b2fdfbd586f7b2ce407027bbe5b
justinzebra <39110422+justinzebra@users.noreply.github.com>
2020-12-02 02:43:12 +0800

    20201202

---
+++ Assets/手術室/手術室腳本/Knifegone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+public class Knifegone : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        private Vector3 screenPoint;
+    private Vector3 offset;
+  void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        //tb.gravityScale = 0;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+   void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Knifegone")
+        {
+            Destroy(this.gameObject);
+             Flowchart.BroadcastFungusMessage ( "刀子消失" );
+            Debug.Log("5566");
+        }
+      
+    }
+}
