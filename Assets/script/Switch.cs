7d9fbeda08fff24a8c3ed74555740e74509e9d06
justinzebra <39110422+justinzebra@users.noreply.github.com>
2020-12-08 02:07:13 +0800

    20201208_WEI

--- Assets/script/Switch.cs
+++ Assets/script/Switch.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Fungus;
 
 public class Switch : MonoBehaviour
 {
-
+    public bool opendoor = false;
     [Header("連接到某場景")]
     public string goToTheScene;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(goToTheScene);
+            opendoor = true;
         }
     }
 
+    IEnumerator WaitBeforeShow()
+    {
+        if (opendoor == true)
+        {
+            Flowchart.BroadcastFungusMessage("0857");
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene(goToTheScene);
+
+        }
+    }
+
+    void Update()
+    {
+        StartCoroutine(WaitBeforeShow());
+    }
 }
