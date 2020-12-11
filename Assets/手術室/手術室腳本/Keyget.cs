4c5910f182d3fcfdaf903f79fef520faded63493
justinzebra <39110422+justinzebra@users.noreply.github.com>
2020-12-11 12:20:26 +0800

    20201211

--- Assets/手術室/手術室腳本/Keyget.cs
+++ Assets/手術室/手術室腳本/Keyget.cs
@@ -7,7 +7,9 @@ public class Keyget : MonoBehaviour
     void OnMouseDown()
     {
         Flowchart.BroadcastFungusMessage ( "鑰匙出現" );
+         Flowchart.BroadcastFungusMessage ( "屍體鑰匙沒有" );
         Debug.Log("0000");
+    
     }
     // Start is called before the first frame update
 
