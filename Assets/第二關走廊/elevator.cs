3d66555c84ca917428e8635f020ff3245452da9d
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2021-02-16 12:44:08 +0800

    2021021612.44

---
+++ Assets/第二關走廊/elevator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum Elevator
+{
+    firstfloor,
+    secondfloor
+}
+
+public class elevator : MonoBehaviour
+{
+    public string sceneName;
+    public Elevator elevatoR;
+    Animator door_animator; 
+    public AudioClip p1;
+    public AudioClip p2;
+    AudioSource audiosource;
+    public elevatorbutton elevatorbutton;
+    public void elevatorfloor()
+    {
+        if(sceneName=="gallery_firstfloor"&&elevatoR==Elevator.firstfloor)
+        {
+            elevatorarrival();
+        }
+        else if(sceneName=="gallery_firstfloor"&&elevatoR==Elevator.secondfloor)
+        {
+            Invoke("elevatorarrival",3f);
+            elevatoR=Elevator.firstfloor;
+        }
+        else if(sceneName=="2"&&elevatoR==Elevator.secondfloor)
+        {
+            Invoke("elevatorarrival",3f);
+            elevatoR=Elevator.firstfloor;
+        }
+        else if(sceneName=="2"&&elevatoR==Elevator.secondfloor)
+        {
+            elevatorarrival();
+        }
+    }
+    void elevatorarrival()
+    {
+        Invoke("elevatorOpen",1f);
+        audiosource.PlayOneShot(p2,1f);
+    }
+    public void elevatorOpen()
+    {
+        door_animator.SetBool("elevatorOpen",true);
+        door_animator.SetBool("elevatorClose",false);
+        Invoke("elevatorClose",7f);
+        audiosource.PlayOneShot(p1,1f);
+    }
+    void elevatorClose()
+    {
+        door_animator.SetBool("elevatorOpen",false);
+        door_animator.SetBool("elevatorClose",true);
+        elevatorbutton.elevatoriswork=false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        door_animator = GetComponent<Animator>();
+        audiosource = GetComponent<AudioSource>();
+        sceneName = SceneManager.GetActiveScene().name;
+        elevatorbutton = elevatorbutton.GetComponent<elevatorbutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // DoorControl();
+    }
+}
---
+++ Assets/第二關走廊/elevatorbutton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevatorbutton : MonoBehaviour
+{
+    public elevator elevator;
+    public bool elevatorcanmove;
+    public bool elevatoriswork;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.name=="player")
+        {
+            elevatorcanmove=true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.name=="player")
+        {
+            elevatorcanmove=false;
+        }
+    }
+    void Openelevator()
+    {
+        if(Input.GetKeyDown(KeyCode.E)&&elevatoriswork==false)
+        {
+            elevator.elevatorfloor();
+            elevatoriswork=true;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        elevator = elevator.GetComponent<elevator>();
+        elevatorcanmove=false;
+        elevatoriswork=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(elevatorcanmove==true)
+        {
+            Openelevator();
+        }
+    }
+}
