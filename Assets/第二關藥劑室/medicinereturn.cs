98dcaa0d222ac3caf2800c47944598790defc268
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2021-02-23 15:07:32 +0800

    202102231507

--- Assets/第二關藥劑室/medicinereturn.cs
+++ Assets/第二關藥劑室/medicinereturn.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class medicinereturn : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
     public bool returnbutton=false;
+    public AudioClip p;
+    AudioSource audioSource;
+    Color c;
     void OnMouseDown()
     {
+        // returnbutton=true;
+        audioSource.PlayOneShot(p);
+        c = new Color(1f,1f,1f,0.5f);
+        this.GetComponent<SpriteRenderer>().color=c;
+    }
+    void OnMouseUp()
+    {
         returnbutton=true;
+        // audioSource.PlayOneShot(p);
+        c = new Color(1f,1f,1f,1f);
+        this.GetComponent<SpriteRenderer>().color=c;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource=GetComponent<AudioSource>();
+        spriteRenderer=GetComponent<SpriteRenderer>();
+        c=spriteRenderer.color;
     }
 
     // Update is called once per frame
