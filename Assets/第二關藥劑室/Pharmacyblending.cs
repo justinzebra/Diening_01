4c8aef79a1008af8bda95101753b279a6bcc5a20
justinzebra <39110422+justinzebra@users.noreply.github.com>
2021-03-16 18:59:48 +0800

    20210316

--- Assets/第二關藥劑室/Pharmacyblending.cs
+++ Assets/第二關藥劑室/Pharmacyblending.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
 public enum ColorS
-    {
-        colorLess,
-        colorWhite,
-        colorYellow,
-        colorBlue,
-        colorGreen,
-        colorRed,
-        colorOrage,
-        colorBlack
-    }
+{
+    colorLess,
+    colorWhite,
+    colorYellow,
+    colorBlue,
+    colorGreen,
+    colorRed,
+    colorOrage,
+    colorBlack
+}
+
+
+
+
 public class Pharmacyblending : MonoBehaviour
 {
     public ColorS colors;
@@ -24,129 +31,144 @@ public class Pharmacyblending : MonoBehaviour
     Color fincolor;
     public Sprite[] medicines;
     public float smooth;
-    public bool rightcolor=false;
+    public bool rightcolor = false;
+    public Flowchart flowchart;
+    string CantakeName = "可拿杯子";
+    public bool Cantake
+    {
+        get
+        {
+            return flowchart.GetBooleanVariable(CantakeName);
+        }
+        set
+        {
+            flowchart.SetBooleanVariable(CantakeName, value);
+        }
+    }
     void OnMouseDrag()
     {
-        if(rightcolor==true)
+        if (rightcolor == true)
         {
-            Camera cam = Camera.main;      
-            Vector3 newPos = cam.ScreenToWorldPoint(Input.mousePosition);  
-            this.transform.position = new Vector3(newPos.x, newPos.y,this.transform.position.z);
+            Camera cam = Camera.main;
+            Vector3 newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            this.transform.position = new Vector3(newPos.x, newPos.y, this.transform.position.z);
         }
     }
     void canMove()
     {
-        rightcolor=true;
+        rightcolor = true;
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "黃色"&&colors==ColorS.colorLess)
+        if (other.gameObject.name == "黃色" && colors == ColorS.colorLess)
         {
-            colors=ColorS.colorYellow;
+            colors = ColorS.colorYellow;
         }
-        else if(other.gameObject.name == "黃色"&&colors==ColorS.colorRed)
+        else if (other.gameObject.name == "黃色" && colors == ColorS.colorRed)
         {
-           colors=ColorS.colorOrage;
-           Invoke("canMove",2f);
+            colors = ColorS.colorOrage;
+            Invoke("canMove", 2f);
+            Cantake = true;
         }
-        else if(other.gameObject.name == "黃色"&&colors!=ColorS.colorLess&&colors!=ColorS.colorYellow)
+        else if (other.gameObject.name == "黃色" && colors != ColorS.colorLess && colors != ColorS.colorYellow)
         {
-            colors=ColorS.colorBlack;
+            colors = ColorS.colorBlack;
         }
-        else if(other.gameObject.name == "藍色"&&colors==ColorS.colorLess)
+        else if (other.gameObject.name == "藍色" && colors == ColorS.colorLess)
         {
-            colors=ColorS.colorBlue;
+            colors = ColorS.colorBlue;
         }
-        else if(other.gameObject.name == "藍色"&&colors!=ColorS.colorLess&&colors!=ColorS.colorBlue)
+        else if (other.gameObject.name == "藍色" && colors != ColorS.colorLess && colors != ColorS.colorBlue)
         {
-            colors=ColorS.colorBlack;
+            colors = ColorS.colorBlack;
         }
-        else if(other.gameObject.name == "白色"&&colors==ColorS.colorLess)
+        else if (other.gameObject.name == "白色" && colors == ColorS.colorLess)
         {
-            colors=ColorS.colorWhite;
+            colors = ColorS.colorWhite;
         }
-        else if(other.gameObject.name == "白色"&&colors!=ColorS.colorLess&&colors!=ColorS.colorWhite)
+        else if (other.gameObject.name == "白色" && colors != ColorS.colorLess && colors != ColorS.colorWhite)
         {
-            colors=ColorS.colorBlack;
+            colors = ColorS.colorBlack;
         }
-        else if(other.gameObject.name == "紅色"&&colors==ColorS.colorLess)
+        else if (other.gameObject.name == "紅色" && colors == ColorS.colorLess)
         {
-            colors=ColorS.colorRed;
+            colors = ColorS.colorRed;
         }
-        else if(other.gameObject.name == "紅色"&&colors==ColorS.colorYellow)
+        else if (other.gameObject.name == "紅色" && colors == ColorS.colorYellow)
         {
-            colors=ColorS.colorOrage;
-            Invoke("canMove",2f);
+            colors = ColorS.colorOrage;
+            Invoke("canMove", 2f);
+            Cantake = true;
         }
-        else if(other.gameObject.name == "紅色"&&colors!=ColorS.colorLess&&colors!=ColorS.colorRed)
+        else if (other.gameObject.name == "紅色" && colors != ColorS.colorLess && colors != ColorS.colorRed)
         {
-            colors=ColorS.colorBlack;
+            colors = ColorS.colorBlack;
         }
-         else if(other.gameObject.name == "綠色"&&colors==ColorS.colorLess)
+        else if (other.gameObject.name == "綠色" && colors == ColorS.colorLess)
         {
-            colors=ColorS.colorGreen;
+            colors = ColorS.colorGreen;
         }
-        else if(other.gameObject.name == "綠色"&&colors!=ColorS.colorLess&&colors!=ColorS.colorGreen)
+        else if (other.gameObject.name == "綠色" && colors != ColorS.colorLess && colors != ColorS.colorGreen)
         {
-            colors=ColorS.colorBlack;
+            colors = ColorS.colorBlack;
         }
-        
+
     }
     void ColorJudgment()
     {
-        if(colors==ColorS.colorWhite)
+        if (colors == ColorS.colorWhite)
         {
             // fincolor = Color.white;
-            spriteRenderer.sprite=medicines[1];
+            spriteRenderer.sprite = medicines[1];
         }
-        else if(colors==ColorS.colorYellow)
+        else if (colors == ColorS.colorYellow)
         {
             // fincolor = color1;
-            spriteRenderer.sprite=medicines[3];
+            spriteRenderer.sprite = medicines[3];
         }
-        else if(colors==ColorS.colorBlue)
+        else if (colors == ColorS.colorBlue)
         {
             // fincolor= color2;
-            spriteRenderer.sprite=medicines[4];
+            spriteRenderer.sprite = medicines[4];
         }
-        else if(colors==ColorS.colorGreen)
+        else if (colors == ColorS.colorGreen)
         {
             // fincolor = color3;
-            spriteRenderer.sprite=medicines[2];
+            spriteRenderer.sprite = medicines[2];
         }
-        else if(colors==ColorS.colorRed)
+        else if (colors == ColorS.colorRed)
         {
             // fincolor = color3;
-            spriteRenderer.sprite=medicines[5];
+            spriteRenderer.sprite = medicines[5];
         }
-        else if(colors==ColorS.colorBlack)
+        else if (colors == ColorS.colorBlack)
         {
             // fincolor = color3;
-            spriteRenderer.sprite=medicines[6];
+            spriteRenderer.sprite = medicines[6];
         }
-         else if(colors==ColorS.colorOrage)
+        else if (colors == ColorS.colorOrage)
         {
             // fincolor = color3;
-            spriteRenderer.sprite=medicines[7];
+            spriteRenderer.sprite = medicines[7];
         }
-         else if(colors==ColorS.colorLess)
+        else if (colors == ColorS.colorLess)
         {
             // fincolor = color3;
-            spriteRenderer.sprite=medicines[0];
+            spriteRenderer.sprite = medicines[0];
         }
         // spriteRenderer.color = Color.Lerp(spriteRenderer.color, fincolor, smooth * Time.deltaTime);
     }
     void MedicineReturn()
     {
-        if(medicinereturn.returnbutton==true)
+        if (medicinereturn.returnbutton == true)
         {
-            colors=ColorS.colorLess;
-            rightcolor=false;
+            colors = ColorS.colorLess;
+            rightcolor = false;
         }
     }
     void ReturnButton()
     {
-        if(colors==ColorS.colorBlack)
+        if (colors == ColorS.colorBlack)
         {
             returnbutton.SetActive(true);
         }
@@ -156,11 +178,19 @@ public class Pharmacyblending : MonoBehaviour
         }
     }
     // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        if (Cantake == true)
+            Flowchart.BroadcastFungusMessage("得到藥水");
+
+        Debug.Log("0000");
+
+    }
     void Start()
     {
-        spriteRenderer= GetComponent<SpriteRenderer>();
-        medicinereturn=medicinereturn.GetComponent<medicinereturn>();
-        colors=ColorS.colorLess;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        medicinereturn = medicinereturn.GetComponent<medicinereturn>();
+        colors = ColorS.colorLess;
         returnbutton.SetActive(false);
     }
 
