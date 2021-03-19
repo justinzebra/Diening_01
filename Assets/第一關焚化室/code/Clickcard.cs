123f55ce174ee1e26ef0c5089e0d71ad6685daa0
justinzebra <39110422+justinzebra@users.noreply.github.com>
2021-03-19 17:33:52 +0800

    20210319

---
+++ Assets/第一關焚化室/code/Clickcard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+
+public class Clickcard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+
+        Flowchart.BroadcastFungusMessage("得到磁卡");
+        Destroy(this.gameObject);
+        Debug.Log("0000");
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
